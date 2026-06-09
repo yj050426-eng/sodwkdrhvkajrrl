dbb23ad4c7d57048dc7ab07f4c6fab524f023130
정유진 <>
2026-06-09 17:35:59 +0900

    update project

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(cors());
 app.use(express.json());
 
 
+// 정적 파일 연결
+app.use(express.static(__dirname));
+
+
 // ===============================
 // 레시피 API
 // ===============================
@@ -25,34 +29,17 @@ app.post("/recipe", async (req, res) => {
 너는 요리 추천 AI다.
 
 사용자가 가진 재료를 기반으로
-여러 개의 레시피를 추천해라.
+레시피 3개를 추천해라.
 
-규칙:
+반드시 JSON 형식만 출력해라.
 
-1. 사용자가 가진 재료와 겹치는 재료가 많은 레시피를 우선 추천
-2. 최소 3개의 레시피 추천
-3. matchScore는 재료 일치율 퍼센트
-4. ownedIngredients에는 사용자가 가진 재료
-5. missingIngredients에는 부족한 재료
-6. steps는 요리 순서
-7. 반드시 JSON 형식만 출력
-8. 설명 문장 금지
-9. 김치가 있으면:
-김치볶음밥,
-김치찌개,
-김치전
-등 다양하게 추천
-
-사용자 재료:
-${ingredients.join(", ")}
-
-JSON 형식:
+형식:
 
 {
   "recipes": [
     {
       "name": "레시피 이름",
-      "description": "레시피 설명",
+      "description": "설명",
       "matchScore": 90,
       "ownedIngredients": [],
       "missingIngredients": [],
@@ -60,6 +47,9 @@ JSON 형식:
     }
   ]
 }
+
+사용자 재료:
+${ingredients.join(", ")}
 
 `;
 
@@ -100,21 +90,13 @@ JSON 형식:
 
     console.log(data);
 
-    
-    // ===============================
-    // 오류 방지
-    // ===============================
-
     if (!data.choices) {
-
-      console.log(data);
 
       return res
         .status(500)
         .send("AI 응답 오류");
 
     }
-
 
     const text =
       data.choices[0].message.content;
@@ -133,7 +115,7 @@ JSON 형식:
 
 
 // ===============================
-// Render 배포용
+// 서버 실행
 // ===============================
 
 const PORT =
